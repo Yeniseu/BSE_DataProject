f81819ab156ab64e5f563220e666b4d41c18063d
Orhun <orhun1337@gmail.com>
2025-12-04 20:55:26 +0100

    fix

--- 01_RScript/10_Lasso_Ridge.R
+++ 01_RScript/10_Lasso_Ridge.R
@@ -44,28 +44,15 @@ elnet_s1_l3 <- lasso_roll_win(dt_s1, npred1, 1, lag=3, alpha=balp1, lambda=best_
 
 ## Sample 2: Train: 1960-01-01:2015-12-01.  Test: 2016-01-01:2024-12-01
 npred2 <- nrow(fred) - fred[, which(date=="2015-12-01")]  # 108 as of 2024-12-01
-#best_lam_all_2 <- get_best_lambda(dt_s1, npred2, 1, lag=1, alpha=1, nlambda=25)  #0.0091218
-#best_lam_2 <- best_lam_all_2$best_lam
-#best_lam_2 <- 0.0091218
 best_lam_2 <- best_lam_1
+balp2      <- balp1
 lasso_s2_l1 <- lasso_roll_win(dt_s2, npred2, 1, lag=1, alpha=1    , lambda=best_lam_2)
 lasso_s2_l3 <- lasso_roll_win(dt_s2, npred2, 1, lag=3, alpha=1    , lambda=best_lam_2)
 ridge_s2_l1 <- lasso_roll_win(dt_s2, npred2, 1, lag=1, alpha=0    , lambda=best_lam_2)
 ridge_s2_l3 <- lasso_roll_win(dt_s2, npred2, 1, lag=3, alpha=0    , lambda=best_lam_2)
-elnet_s2_l1 <- lasso_roll_win(dt_s2, npred2, 1, lag=1, alpha=balp1, lambda=best_lam_2)
-elnet_s2_l3 <- lasso_roll_win(dt_s2, npred2, 1, lag=3, alpha=balp1, lambda=best_lam_2)
+elnet_s2_l1 <- lasso_roll_win(dt_s2, npred2, 1, lag=1, alpha=balp2, lambda=best_lam_2)
+elnet_s2_l3 <- lasso_roll_win(dt_s2, npred2, 1, lag=3, alpha=balp2, lambda=best_lam_2)
 
-
-ridge_s2_l3$real
-lasso_s2_l3$real
-sapply(elnet_s2_l1, function(x) x$errors)
-writeClipboard(as.character(sapply(elnet_s2_l1, function(x) x$errors)[1,]))
-
-
-
-best_lam_all_1$all_res[[1]]$lambda
-best_lam_all_1$all_res[[1]]$errors
-sum(best_lam_all_1$all_res[[1]]$coef[1,] != 0)
 
 ### Create Charts
 ## Lambda Grid Search
